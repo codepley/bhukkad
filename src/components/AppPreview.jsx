6344be5ec3dd5676c994bd931959efa18c06fee5
codepley <officialkushalkaran@gmail.com>
2024-01-13 16:39:43 +0530

    phone design

--- src/components/AppPreview.jsx
+++ src/components/AppPreview.jsx
@@ -1,14 +1,21 @@
 const AppPreview = () => {
   return <div className="w-full flex justify-center items-center pb-16">
    <div className="w-full lg:w-[1440px] flex flex-col gap-16 justify-center items-center">
-      <div className="relative w-[700px] flex justify-center items-center">
-         <img className="z-[1] absolute w-[299px] h-[586px] top-[-160px] right-[300px] flex-shrink-0 object-cover drop-shadow-2xl" src="/left.png" alt="" />
-         <img className="absolute w-[299px] top-[-100px] right-[80px] h-[586px] flex-shrink-0 object-cover drop-shadow-2xl" src="right.png" alt="" />
+      <div className="relative w-full lg:w-[700px] flex justify-center items-center">
+         <img className="-mt-28 md:-mt-40" src="/phone.png" alt="" />
+         {/* <img className="hidden sm:flex z-[1] absolute w-[299px] h-[586px] top-[-160px] right-[300px] object-cover drop-shadow-2xl" src="/left.png" alt="" />
+         <img className="hidden sm:flex absolute w-[299px] top-[-100px] right-[80px] h-[586px] object-cover drop-shadow-2xl" src="right.png" alt="" /> */}
       </div>
-      <div className="h-[3px] w-full lg:w-[884px] bg-[#E4E4E4] hidden lg:flex lg:mt-[500px]"></div>
-      <div className="text-4xl font-bold text-center tracking-wider lg:mt-0 mt-[500px]">How the app works</div>
+      <div className="h-[3px] w-full lg:w-[884px] bg-[#E4E4E4] hidden lg:flex lg:mt-16"></div>
+      <div className="text-4xl font-bold text-center tracking-wider lg:mt-0 mt-16">How the app works</div>
       <div className="lg:hidden h-[3px] w-full lg:w-[884px] bg-[#E4E4E4]"></div>
    </div>
   </div>;
 };
 export default AppPreview;
+
+
+{/* <div className="relative w-[700px] flex justify-center items-center">
+         <img className="z-[1] absolute w-[299px] h-[586px] top-[-160px] right-[300px] flex-shrink-0 object-cover drop-shadow-2xl" src="/left.png" alt="" />
+         <img className="absolute w-[299px] top-[-100px] right-[80px] h-[586px] flex-shrink-0 object-cover drop-shadow-2xl" src="right.png" alt="" />
+      </div> */}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,11 @@ import Buttons from "./Buttons";
 
 const Hero = () => {
   return (
-    <div className="w-full flex justify-center items-center">
-      <div
-        style={{
-          background: `linear-gradient(0deg, rgba(23, 16, 16, 0.60) 0%, rgba(23, 16, 16, 0.60) 100%), url('/heroBg.jpeg'), lightgray 50% / cover no-repeat`,
-          backgroundSize: "cover",
-        }}
-        className="lg:w-[1440px] w-full h-[620px] text-white flex flex-col justify-center items-center gap-10"
+    <div style={{
+      background: `linear-gradient(0deg, rgba(23, 16, 16, 0.60) 0%, rgba(23, 16, 16, 0.60) 100%), url('/heroBg.jpeg'), lightgray 50% / cover no-repeat`,
+      backgroundSize: "cover",
+    }} className="w-full flex justify-center items-center">
+      <div className="lg:w-[1440px] w-full h-[620px] text-white flex flex-col justify-center items-center gap-10"
       >
         <div className="lg:w-[1004px] w-full font-poppins flex flex-col justify-center gap-6 items-center">
           <div className="font-extrabold flex flex-col lg:gap-2 text-4xl lg:text-6xl text-center">
